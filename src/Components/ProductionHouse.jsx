562756a4af2b8d83f6d7429a406d0cb675b0a615
osama <usamaramadan33@gmail.com>
2025-10-16 05:26:35 +0300

    Initial commit - Disney Clone project

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -21,7 +21,7 @@ export const ProductionHouse = () => {
   return (
     <div className='flex justify-center items-center flex-wrap mt-8 mb-8'>
       {productionHouses.map((house) => (
-        <div key={house.name} className="group gap-4 relative w-80 h-48 cursor-pointer overflow-hidden border-2 border-gray-300 rounded-lg m-4 transition-transform transform hover:scale-105">
+        <div key={house.name} className="group gap-4 relative w-60 cursor-pointer overflow-hidden border-2 border-gray-300 rounded-lg m-4 transition-transform transform hover:scale-105">
           <img src={house.image} alt={house.name} className="w-full h-full object-cover z-[1]" />
             <video className="absolute top-0 left-0 w-full h-full object-cover opacity-0 group-hover:opacity-40 transition-opacity duration-300 z-[0]" autoPlay loop muted>
               <source src={house.video} type="video/mp4" />
